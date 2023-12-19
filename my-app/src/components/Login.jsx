3ca7aea0d0f00f04a789f47b96d62a38c51592e7
Yaddala Hrudayesh <113190081+Hrudayesh-yaddala@users.noreply.github.com>
2023-12-19 18:53:12 +0530

    Update Login.jsx

--- my-app/src/components/Login.jsx
+++ my-app/src/components/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        // "http://localhost:8000/api/user/login",
+  
         "https://major-backend-infiniti.onrender.com/api/user/login",
         formData,
         {
